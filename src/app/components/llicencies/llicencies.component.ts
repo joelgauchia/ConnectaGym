0d7c3257c8ff0a8d4551092e8e2b7d94e592e23b
Joel Gauchia <joelgauchia@gmail.com>
2024-04-29 16:28:52 +0200

    Al eliminar llicencia, es posa el membre inactiu

--- src/app/components/llicencies/llicencies.component.ts
+++ src/app/components/llicencies/llicencies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ExcelService } from '../../services/excel.service';
 import { Llicencia } from '../../models/llicencia.model';
@@ -6,6 +6,9 @@ import { LlicenciesService } from '../../services/llicencies.service';
 import { forkJoin } from 'rxjs';
 import { TipusLlicenciaService } from '../../services/tipusLlicencia.service';
 import { TipusLlicencia } from '../../models/tipus-llicencia.model';
+import { Usuari } from '../../models/usuari.model';
+import { UsuarisService } from '../../services/usuaris.service';
+import { PropietarisService } from '../../services/propietaris.service';
 
 @Component({
   selector: 'app-llicencies',
@@ -27,6 +30,7 @@ export class LlicenciesComponent implements OnInit {
   constructor(
     private llicenciesService: LlicenciesService,
     private tipusLlicenciaService: TipusLlicenciaService,
+    private usuarisService: UsuarisService,
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
     private excelService: ExcelService
@@ -80,11 +84,15 @@ export class LlicenciesComponent implements OnInit {
       icon: 'pi pi-exclamation-triangle',
       accept:() => {
         this.llicenciesService.eliminarLlicencia(event).subscribe(response => {
-          this.messageService.add({ severity: 'success', summary: 'Fet!', detail: response, life: 3000 });
-          this.carregarLlicencies();
+          const usuari: Usuari = event.propietari.creador;
+          usuari.actiu = false; 
+          this.usuarisService.actualitzarUsuari(usuari.nomUsuari, usuari).subscribe(() => {
+            this.messageService.add({ severity: 'success', summary: 'Fet!', detail: response, life: 3000 });
+            this.carregarLlicencies();
+          });
         })
       }
-    })
+    });
   }
 
   imprimirRebut(): void {
