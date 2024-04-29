2dde14ebc1ff84661c6c0f7fdf16bb21eb07722d
Joel Gauchia <joelgauchia@gmail.com>
2024-04-29 16:39:50 +0200

    Treta opcio de register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
 import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [LoginGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
